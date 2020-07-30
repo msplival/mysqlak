40aaa3926bf6cf0bfc28de1e2f2c8bb82da6a06b
Mario Splivalo <mario@splivalo.hr>
2020-07-30 22:57:46 +0200

    Added logging of duration for each commit

--- mysqlak.py
+++ mysqlak.py
@@ -2,6 +2,7 @@
 
 import mysql.connector
 import uuid
+import time
 
 
 def do_batch(size=10):
@@ -19,13 +20,17 @@ def do_batch(size=10):
     curs.execute("INSERT INTO batches (batch_uuid) VALUES ('{0}');".format(batch_uuid))
     conn.commit()
 
-    for i in range(1,size):
-        sql = f"INSERT INTO t1 (batch_uuid, data_value) VALUES ('{batch_uuid}', 'data-{i}');"
-        curs.execute(sql)
-        conn.commit()
+    with open(f'batch-{batch_uuid}.txt', 'a') as logFile:
+        for i in range(1,size):
+            sql = f"INSERT INTO t1 (batch_uuid, data_value) VALUES ('{batch_uuid}', 'data-{i}');"
+            tic = time.perf_counter()
+            curs.execute(sql)
+            conn.commit()
+            toc = time.perf_counter()
+            logFile.write(f'Counter: {i}, duration: {toc - tic} seconds. \n')
 
     conn.close()
     print('   Completed.')
 
-for i in range(1,100, 10):
+for i in range(1,2, 10):
     do_batch(i*100)
