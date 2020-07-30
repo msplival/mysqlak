27fb1ccab906262e0c370d5e5de91917e7832771
Mario Splivalo <mario@splivalo.hr>
2020-07-30 22:48:08 +0200

    TIL: f-strings

--- mysqlak.py
+++ mysqlak.py
@@ -7,7 +7,7 @@ import uuid
 def do_batch(size=10):
     batch_uuid = uuid.uuid4()
 
-    print('Starting batch {0}, size {1}'.format(batch_uuid, size))
+    print(f'Starting batch {batch_uuid}, size {size}')
     print('   Connecting to database...')
     conn = mysql.connector.connect(user='mario', 
                                 host='xen-mysql-percona.lxd',
@@ -20,7 +20,7 @@ def do_batch(size=10):
     conn.commit()
 
     for i in range(1,size):
-        sql = "INSERT INTO t1 (batch_uuid, data_value) VALUES ('{0}', 'data-{1}');".format(batch_uuid, i)
+        sql = f"INSERT INTO t1 (batch_uuid, data_value) VALUES ('{batch_uuid}', 'data-{i}');"
         curs.execute(sql)
         conn.commit()
 
